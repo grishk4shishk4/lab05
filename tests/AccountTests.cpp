91e72d251a4d38a034b26abf6cdcddf1d83c2549
grishk4shishk4 <gregoriymukhin@yandex.com>
2026-05-13 15:21:19 +0300

    Changed .github directory

--- tests/AccountTests.cpp
+++ tests/AccountTests.cpp
@@ -2,27 +2,39 @@
 #include <gmock/gmock.h>
 #include "Account.h"
 
+
 TEST(AccountTests, GetBalance)
 {
-	const int balance = 500;
-	Account acc(01, balance);
-	const int result = acc.GetBalance();
-	EXPECT_EQ(balance, result);
+    const int balance = 500;
+    Account acc(1, balance);
+    const int result = acc.GetBalance();
+    EXPECT_EQ(balance, result);
 }
 
 TEST(AccountTests, ChangeBalance)
 {
-	const int balance = 850;
-	const int deposit_sum = 150;
-	Account acc(02, balance);
-	acc.ChangeBalance(sum);
-	EXPECT_EQ((balance + sum), acc.GetBalance());
+    const int balance = 850;
+    const int sum = 150;
+    Account acc(2, balance);
+    acc.Lock();
+    acc.ChangeBalance(sum);
+    acc.Unlock();
+    EXPECT_EQ((balance + sum), acc.GetBalance());
 }
 
-TEST(AccountTests, Lock)
+TEST(AccountTests, Lock_locked)
 {
-	Account acc(03, 1000);
-	acc.Lock();
-	EXPECT_THROW({acc.Lock();}, std::runtime_error);
+    const int balance = 1000;
+    Account acc(3, balance);
+    acc.Lock();
+    EXPECT_THROW({acc.Lock();}, std::runtime_error);
+
 }
 
+TEST(AccountTests, ChangeBalance_unlocked)
+{
+    const int balance = 1000;
+    Account acc(1, balance);
+    acc.Unlock();   
+    EXPECT_THROW({acc.ChangeBalance(100);}, std::runtime_error);
+}
